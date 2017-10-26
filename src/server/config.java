5852ff56768f53c852f4a333fadee91cb29a26c4
muditaggarwal2001 <muditaggarwal2001.muditaggarwal2001@gmail.com>
2017-10-26 10:09:25 -0400

    Bug fixed - Complete
    Huzzah again

--- src/server/config.java
+++ src/server/config.java
@@ -5,7 +5,7 @@ public class config {
 	public static final int MaxCourseforPT = 2;
 	public static final int MaxCourseforFT = 4;
 	public static final int MinCapsize = 26;
-	public static final int clerktime = 400000;
-	public static final long Termend = 2360000;
-	public static final long RegisterDeadline = 960000;
+	public static final int clerktime = 300000;
+	public static final long Termend = 720000;
+	public static final long RegisterDeadline = 480000;
 }
